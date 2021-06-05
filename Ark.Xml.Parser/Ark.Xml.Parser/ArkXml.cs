406a2d8c625434b44f1df173005e53ebc79c4eab
Immanuel <raj@immanuel.co>
2021-06-06 03:24:49 +0530

    base changes

---
+++ Ark.Xml.Parser/Ark.Xml.Parser/ArkXml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ark.Xml.Parser
+{
+    class ArkXml
+    {
+        static void Info(string msg)
+        {
+            //Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+        static void Error(Exception exp)
+        {
+            //Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(exp);
+            Console.ResetColor();
+        }
+        string _content;
+        XDocument doc = null;
+        void Validate(string content)
+        {
+            try
+            {
+                doc = XDocument.Parse(content);
+            }
+            catch (System.Xml.XmlException xexp)
+            {
+                Error(xexp);
+                return;
+            }
+        }
+        public void Add(string ele)
+        {
+            doc.Root.Add(XElement.Parse(ele));
+        }
+        public override string ToString()
+        {
+            return doc.ToString();
+        }
+        public bool EnableInfo { get; set; } = true;
+        public ArkXml(string content)
+        {
+            Validate(content);
+            _content = content;
+            Info($"Received Content : {Environment.NewLine}{content}");
+        }
+    }
+}
---
+++ Ark.Xml.Parser/Ark.Xml.Parser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ark.Xml.Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string cont = "invalid xml";
+            string file_name = args != null && args.Length > 0 ? args[0] : "csproj_xml1.xml";
+            string cont = System.IO.File.ReadAllText(file_name);
+            var tg = new Ark.Xml.Parser.ArkXml(cont);
+            tg.Add("<ItemGroup><Compile Remove=\"Migrations\\**\" /><EmbeddedResource Remove = \"Migrations\\**\" /><None Remove = \"Migrations\\**\" /></ItemGroup>");
+            System.IO.File.WriteAllText(file_name, tg.ToString());
+            //Console.WriteLine(tg.ToString());
+        }
+    }
+}
